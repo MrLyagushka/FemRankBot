6e0ac2a6050b0cabfb4c925deaf03ef74ffe8cad
Oleg <olegc601@gmail.com>
2026-08-08 02:47:25 +0300

    debug

--- app/handlers/handler_download_a_photos.py
+++ app/handlers/handler_download_a_photos.py
@@ -46,7 +46,7 @@ async def download_photos_start(message: Message, state: FSMContext, bot: Bot):
             await db.close()
             await state.set_state(DownloadPhotos.successfuly)
             db = DB_Group(PATH_TO_DB_DATA)
-            group = db.get_groups()
+            group = await db.get_groups()
             print(group)
             if group != []:
                 await message.answer('Выберите счастливчиков: ', reply_markup= await DinamicKeyboard(1, 3, 'no', 0, f'groups_{message.from_user.id}').generate_keyboard())
