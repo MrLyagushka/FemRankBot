765279360ed11ed37c80a451a7cabed7ee5435cf
Oleg <olegc601@gmail.com>
2026-08-08 05:06:11 +0300

    debug

--- app/handlers/handler_groups.py
+++ app/handlers/handler_groups.py
@@ -47,16 +47,16 @@ async def groups_groupssetting(callback: CallbackQuery, state: FSMContext):
     logging.info(group)
 
     await callback.message.edit_text(inline_message_id=callback.inline_message_id, text=f"Группа '{group['full_name']}'\nТип группы: {group['type']}\nId группы: {group['id']}\nСтатус бота в группе: {group['status']}")
+    if group['on_off'] == '1':
+        db.update_group(group['id'], group['type'], group['status'], group['full_name'], group['on_off'])
+        keyboard_groups_choice.update_button('✅Включено', '❌Выключено')
+    elif group['on_off'] == '0':
+        db.update_group(group['id'], group['type'], group['status'], group['full_name'], group['on_off'])
+        keyboard_groups_choice.update_button('❌Выключено', '✅Включено')
     try:
-        if group['on_off'] == '1':
-            db.update_group(group['id'], group['type'], group['status'], group['full_name'], group['on_off'])
-            keyboard_groups_choice.update_button('✅Включено', '❌Выключено')
-        elif group['on_off'] == '0':
-            db.update_group(group['id'], group['type'], group['status'], group['full_name'], group['on_off'])
-            keyboard_groups_choice.update_button('❌Выключено', '✅Включено')
+        await callback.message.edit_reply_markup(inline_message_id=callback.inline_message_id, reply_markup=keyboard_groups_choice.markup)
     except Exception as e:
         pass
-    await callback.message.edit_reply_markup(inline_message_id=callback.inline_message_id, reply_markup=keyboard_groups_choice.markup)
 
 @router_groups.callback_query(F.data[:12] == 'groupsstatus')
 async def groups_groupssetting(callback: CallbackQuery, state: FSMContext):
@@ -74,7 +74,7 @@ async def groups_groupssetting(callback: CallbackQuery, state: FSMContext):
         for x in await db.get_groups()
         if x['id'] == data
     ]
-    
+    logging.info(group)
     if group['on_off'] == '1':
         db.update_group(group['id'], group['type'], group['status'], group['full_name'], group['on_off'])
         keyboard_groups_choice.update_button('✅Включено', '❌Выключено')
