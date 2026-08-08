18515d2f99f31582380b790450ca99a9706fda10
Oleg <olegc601@gmail.com>
2026-08-08 06:01:46 +0300

    debug

--- app/handlers/handler_start.py
+++ app/handlers/handler_start.py
@@ -14,7 +14,8 @@ async def router_start_start(message: Message):
     if message.chat.type == 'private':
         await message.answer_photo(photo="AgACAgIAAxkBAANTam0jEiPZAaUNyQKKOd9gCt6leJwAAr4aaxv_72lLbpKSR281WukBAAMCAAN5AAM9BA")
         db = DB_User(PATH_TO_DB_DATA)
-        db.new_user(message.from_user.id, message.from_user.username, message.from_user.first_name, message.from_user.last_name)
+        await db.new_user(message.from_user.id, message.from_user.username, message.from_user.first_name, message.from_user.last_name)
+        await db.close()
         if message.from_user.id in ADMIN_ID:
             await message.answer("Приветики, мы уже почти закончили, остались только рейтинги фембоев)", reply_markup=keyboard_start_admin.markup)
         else:
