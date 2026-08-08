f7a455b8960b3c622a90a3d66f741039cd562cda
Oleg <olegc601@gmail.com>
2026-08-08 04:03:41 +0300

    debug

--- app/handlers/handler_groups.py
+++ app/handlers/handler_groups.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message, CallbackQuery
 
-from app.keyboards.keyboard_groups import keyboard_groups_start
+from app.keyboards.keyboard_groups import keyboard_groups_start, keyboard_groups_choice
 from app.keyboards.keyboard_my_photos import keyboard_my_photos_first_choice
 from app.utils.dinamic_keyboard import DinamicKeyboard
 from app.utils.utils_group import DB_Group
@@ -29,4 +29,9 @@ async def groups_start(message: Message, state: FSMContext):
 async def groups_groupssetting(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
 
-    logging.info(callback.data.split(' ')[1].split('_')[1])
+    db = DB_Group(PATH_TO_DB_DATA)
+    group = await db.get_groups()
+    logging.info(group)
+    await callback.message.edit_text(inline_message_id=callback.inline_message_id, text="Выберите действие")
+    await callback.message.edit_reply_markup(inline_message_id=callback.inline_message_id, reply_markup=keyboard_groups_choice.markup)
+
--- app/keyboards/keyboard_groups.py
+++ app/keyboards/keyboard_groups.py
@@ -2,3 +2,6 @@ from app.utils.menu import Menu
 
 keyboard_groups_start = Menu('inline', 1)
 keyboard_groups_start.new_button('В разработке', 1, callback_data="groups")
+
+keyboard_groups_choice = Menu('inline', 1)
+keyboard_groups_choice.new_button('✅Включено', 1, callback_data="groupsstatus_on")
